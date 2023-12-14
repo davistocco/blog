41ed76d71543ddf684e421ca78802322244cee1c
Davi Stocco <davistocco@outlook.com>
2023-12-13 23:39:31 -0300

    db config with prisma and posts api get

---
+++ app/api/posts/route.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+    try {
+        const posts = await prisma.posts.findMany();
+        return NextResponse.json(posts, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
