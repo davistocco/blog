bc952611fc3a5ba4caa9a3badeb728b61a20e50a
Davi Stocco <davistocco@outlook.com>
2023-12-14 00:11:51 -0300

    feat: create posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,11 +3,21 @@ import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request) {
+export async function GET() {
     try {
         const posts = await prisma.posts.findMany();
         return NextResponse.json(posts, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ error }, { status: 500 });
+        return NextResponse.json(error, { status: 500 });
+    }
+}
+
+export async function POST(req: Request) {
+    try {
+        const data = await req.json();
+        const post = await prisma.posts.create({ data });
+        return NextResponse.json(post, { status: 201 });
+    } catch (error) {
+        return NextResponse.json(error, { status: 500 });
     }
 }
