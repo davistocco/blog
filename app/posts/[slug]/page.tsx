d7a9c6062660785b043d153afae1305b0749d76e
Davi Stocco <49536789+davistocco@users.noreply.github.com>
2023-12-17 02:34:07 -0300

    Update page.tsx
    feat: disabling post cache revalidate

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { vscDarkPlus as dark } from 'react-syntax-highlighter/dist/esm/styles/pr
 
 export const dynamic = 'force-static';
 export const dynamicParams = true;
-export const revalidate = 3600;
+export const revalidate = false;
 
 export async function generateStaticParams() {
   const posts = await getPosts();
