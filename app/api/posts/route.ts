d0b245a7de56eec7ce46d801194abcf4f3225756
Davi Stocco <49536789+davistocco@users.noreply.github.com>
2023-12-16 21:04:23 -0300

    Update route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
-export const forceDynamic = true
+export const dynamic = 'force-dynamic';
 const prisma = new PrismaClient();
 
 export async function GET() {
