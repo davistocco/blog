108b68f12b5f6c7ee74e4ebf6c2605e7226203dd
Davi Stocco <davistocco@outlook.com>
2023-12-20 16:47:18 -0300

    feat: custom default error page

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Error() {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <p>Algo de errado aconteceu</p>
+        </div>
+    )
+}
