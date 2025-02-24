fb90a98a7a7f02c1161d7bdc97b03edbd07b4a4b
Davi Stocco <davistocco@outlook.com>
2025-02-24 00:09:14 -0300

    separate gifs by column

--- app/gifs/page.tsx
+++ app/gifs/page.tsx
@@ -1,38 +1,30 @@
-import DateLinkItem from "@/components/date-link-item/component";
 import Header from "@/components/header/component";
 import { Metadata } from "next";
-import Image from 'next/image'
+import Image from 'next/image';
+import fs from 'fs';
+import styles from './page.module.css';
 
 export const metadata: Metadata = { title: 'Gifs' };
 export const dynamic = 'force-static';
 
 export default function Gifs() {
-  const gifs = [
-    {
-      src: '/images/larry-david-pretty-good.gif',
-      alt: 'larry david pretty good'
-    },
-  ]
+  const gifFileNames = fs.readdirSync(`${process.cwd()}/public/gifs-page`);
 
   return (
     <main>
-      <Header
-        title='Gifs'
-        createdAt='23/02/2025 12:47'
-      />
-      <ul>
-        {
-          gifs.map((gif, index) => (
-            <Image
-              key={index}
-              src={gif.src}
-              layout="fill"
-              objectFit="contain"
-              alt={gif.alt}
-            />
-          ))
-        }
-      </ul>
+      <Header title="Gifs" createdAt="23/02/2025 12:47" />
+      <div className={`gifsContainer ${styles.gifsContainer}`}>
+        {gifFileNames.map((gif, index) => (
+          <Image
+            className={`${styles.gif}`}
+            key={index}
+            src={`/gifs-page/${gif}`}
+            alt={gif}
+            height={0}
+            width={0}
+          />
+        ))}
+      </div>
     </main>
   );
 }
