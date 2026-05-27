a7633264f172fe9bebbbb56601d46c644de6607b
Davi Stocco <davistocco@outlook.com>
2026-05-27 09:39:57 -0300

    next config base paths

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const repoName = process.env.NEXT_PUBLIC_PAGES_BASE_PATH || '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/blog"
+  basePath: repoName,
+  assetPrefix: repoName
 }
 
 module.exports = nextConfig
